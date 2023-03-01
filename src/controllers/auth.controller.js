91cb97584775bff818a10b31acc64f55f82814ec
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-03-01 07:53:12 -0300

    fix: POST /signin incorrect email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,8 +21,10 @@ export async function signIn(req, res) {
     const { email, password } = req.body
     try {
         const emailExists = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+        if (emailExists.rows.length === 0) return res.status(401).send("Usuário/senha incorretos!")
         const isCorrectPassword = bcrypt.compareSync(password, emailExists.rows[0].password)
-        if (emailExists.rows.length === 0 || !isCorrectPassword) return res.status(401).send("Usuário/senha incorretos!")
+        if (!isCorrectPassword) return res.status(401).send("Usuário/senha incorretos!")
+
 
         const token = uuidV4()
         await db.query(`INSERT INTO sessions(user_email, token) VALUES ($1, $2);`, [email, token])
