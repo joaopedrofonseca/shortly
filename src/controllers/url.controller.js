469ea6ab3afe8a1823b2d14e445f97d503a02056
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-03-01 11:01:07 -0300

    feat: GET /urls/:id

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -7,14 +7,26 @@ export async function shortUrl(req, res) {
     const token = authorization?.replace("Bearer ", "")
     const shortUrl = nanoid(8)
 
-    try{
+    try {
         const isTokenActive = await db.query(`SELECT * FROM sessions WHERE token = $1;`, [token])
-        if(isTokenActive.rows.length === 0) return res.sendStatus(401)
+        if (isTokenActive.rows.length === 0) return res.sendStatus(401)
 
         await db.query(`INSERT INTO urls(url, "shortUrl", "userId") VALUES ($1, $2, $3);`, [url, shortUrl, isTokenActive.rows[0].id])
         const lastUrl = await db.query(`SELECT id,"shortUrl" FROM urls WHERE "shortUrl" = $1;`, [shortUrl])
         return res.status(201).send(lastUrl.rows[0])
-    }catch(err){
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+export async function getUrlById(req, res) {
+    const { id } = req.params
+
+    try{
+        const url = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE id = $1;`, [id])
+        if (url.rows.length === 0) return res.sendStatus(404)
+        return res.status(200).send(url.rows[0])    
+    } catch(err){
         return res.status(500).send(err.message)
     }
 }
--- src/routes/url.router.js
+++ src/routes/url.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { shortUrl } from "../controllers/url.controller.js"
+import { getUrlById, shortUrl } from "../controllers/url.controller.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import shortUrlSchema from "../schemas/shortUrlSchema.js"
 
 const urlRouter = Router()
 
 urlRouter.post('/urls/shorten', validateSchema(shortUrlSchema), shortUrl)
+urlRouter.get('/urls/:id', getUrlById)
 
 export default urlRouter
